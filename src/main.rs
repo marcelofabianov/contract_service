61236c90ed052e5b73f37cb66e12cdc845cb1467
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-04 07:04:49 -0300

    feat: atualizar main.rs para iniciar servidor grpc e construir os containers para controllers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod bootstrap;
 mod db;
 mod env;
+mod grpc {
+    pub mod controller;
+    pub mod service {
+        tonic::include_proto!("customer_grpc_service");
+    }
+}
 mod internal {
     pub mod adapters;
     pub mod domain {
@@ -14,7 +20,9 @@ mod internal {
 use bootstrap::CustomerContainer;
 use db::Postgres;
 use env::Env;
-use internal::domain::use_case::CreateCustomerInput;
+use grpc::controller::CustomerController;
+use grpc::service::customer_service_server::CustomerServiceServer;
+use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,16 +30,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = Postgres::new(&env.database_url).await?;
 
     let container = CustomerContainer::new(env, db).await?;
-    let service = container.create_customer_service().await;
 
-    let input = CreateCustomerInput {
-        document: "12345678901".to_string(),
-        name: "Rust Foundation".to_string(),
-    };
+    let addr = "[::1]:50051".parse().unwrap();
+    let customer_service = CustomerController { container };
 
-    let customer = service.create_customer(input).await?;
+    println!("Server listening on {}", addr);
 
-    println!("{:?}", customer);
+    Server::builder()
+        .add_service(CustomerServiceServer::new(customer_service))
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
