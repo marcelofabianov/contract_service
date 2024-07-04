f493b66e6d15ac3af7215ddea02062ec42d14334
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-04 07:02:23 -0300

    chore: adicionar dependencias de post-types para controllers grpc

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .out_dir("src/gprc/service")
-        .compile(&["proto/customer.proto"], &["proto"])?;
+        .out_dir("./src/grpc/service")
+        .compile(&["./proto/customer.proto"], &["proto"])
+        .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
+
     Ok(())
 }
