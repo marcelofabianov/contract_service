a6208a0018abd2b094a49e5bcac5767caf0e651e
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-08 07:31:32 -0300

    chore: tratar errors de env e adicionar var KAFKA_BROKERS

--- src/env.rs
+++ src/env.rs
@@ -1,25 +1,34 @@
 use dotenv::dotenv;
 use std::env;
+use std::error::Error;
+use uuid::Uuid;
 
+#[derive(Debug, Clone)]
 pub struct Env {
     pub database_url: String,
     pub producer_id: uuid::Uuid,
     pub producer_name: String,
     pub environment: String,
+    pub kafka_brokers: String,
 }
 
 impl Env {
-    pub fn load() -> Self {
+    pub fn load() -> Result<Self, Box<dyn Error>> {
         dotenv().ok();
 
-        Env {
-            database_url: env::var("DATABASE_URL").expect("DATABASE_URL must be set"),
-            producer_id: uuid::Uuid::parse_str(
-                &env::var("PRODUCER_ID").expect("PRODUCER_ID must be set"),
-            )
-            .expect("PRODUCER_ID must be a valid UUID"),
-            producer_name: env::var("PRODUCER_NAME").expect("PRODUCER_NAME must be set"),
-            environment: env::var("ENVIRONMENT").expect("ENVIRONMENT must be set"),
-        }
+        let database_url = env::var("DATABASE_URL")?;
+        let producer_id = Uuid::parse_str(&env::var("PRODUCER_ID")?)
+            .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        let producer_name = env::var("PRODUCER_NAME")?;
+        let environment = env::var("ENVIRONMENT")?;
+        let kafka_brokers = env::var("KAFKA_BROKERS")?;
+
+        Ok(Env {
+            database_url,
+            producer_id,
+            producer_name,
+            environment,
+            kafka_brokers,
+        })
     }
 }
