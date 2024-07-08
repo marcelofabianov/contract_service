6f1080589b96d489d6160783b9cf6fc14f72b2b2
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-07-08 07:40:58 -0300

    chore: removendo customer_container e definindo na raiz container

--- src/container.rs
+++ src/container.rs
@@ -1,29 +1,38 @@
+use std::fmt::Error;
+
 use crate::db::Postgres;
 use crate::env::Env;
 use crate::internal::adapters::CustomerRepositoryPostgres;
 use crate::internal::application::CustomerService;
 use crate::internal::domain::use_case::CreateCustomerUseCase;
+use crate::kafka::Producer;
 
-pub struct CustomerContainer {
+pub struct Container {
     pub env: Env,
     pub db: Postgres,
+    pub producer: Producer,
 }
 
-impl CustomerContainer {
-    pub async fn new(env: Env, db: Postgres) -> Result<Self, Box<dyn std::error::Error>> {
-        Ok(Self { env, db })
+impl Container {
+    pub async fn new(
+        env: Env,
+        db: Postgres,
+        producer: Producer,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(Self { env, db, producer })
     }
 
-    pub async fn create_customer_service(&self) -> CustomerService {
+    pub async fn create_customer_service(&self) -> Result<CustomerService, Error> {
         let pool = self.db.get_pool().await;
         let repository = CustomerRepositoryPostgres::new(pool.clone());
         let use_case = CreateCustomerUseCase::new(repository);
 
-        CustomerService::new(
-            use_case,
+        Ok(CustomerService::new(
             self.env.producer_id,
             self.env.producer_name.clone(),
             self.env.environment.clone(),
-        )
+            self.producer.clone(),
+            use_case,
+        ))
     }
 }
